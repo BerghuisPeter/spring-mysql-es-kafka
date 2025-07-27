43d4f8a186139c9e4b3ded8d6de3c16cc832afe5
peter <peter.berghuis_13@hotmail.com>
2025-07-27 17:17:30 +0900

    init

---
+++ src/main/java/io/github/peterberghuis/springmysqleskafka/SpringMysqlEsKafkaApplication.java
@@ -0,0 +1,13 @@
+package io.github.peterberghuis.springmysqleskafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMysqlEsKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMysqlEsKafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/peterberghuis/springmysqleskafka/SpringMysqlEsKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.peterberghuis.springmysqleskafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMysqlEsKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
